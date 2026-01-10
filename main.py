ce69c4fc9156a5d559ff43ea5982925be789480b
sashabelooov <beloovsasha23@gmail.com>
2026-01-10 23:27:09 +0500

    create file users and wrote function to create user

--- main.py
+++ main.py
@@ -1,115 +1,35 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, select
-from pydantic import BaseModel
-from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as user_router
 
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
-
-
-
-Base = declarative_base()
-
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-
-
-
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-
-
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: str
-    model_config = {"from_attributes": True}
-
-async def get_db():
-    async with AsyncSessionLocal() as db:
-        yield db
-
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await create_tables()
-    yield
-
-
-
-app = FastAPI(lifespan=lifespan)
-
-
-
-@app.post("/users/", response_model=UserResponse)
-async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
-    new_user = User(name=user.name, email=user.email)
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
-    return new_user
-
-
-
-@app.get("/users/", response_model=list[UserResponse])
-async def get_users(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(User))
-    return result.scalars().all()
-
-
-
-@app.get("/users/{user_id}", response_model=UserResponse)
-async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalar_one_or_none()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(user_router)
 
 
 
 
-@app.put("/users/{user_id}", response_model=UserResponse)
-async def update_user(user_id: int, user_data: UserCreate, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(User).where(User.id == user_id))
-    db_user = result.scalar_one_or_none()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+@app.get("/")
+def hello_user():
+    return {
+        "message": "First function hello!"
+    }
     
-    db_user.name = user_data.name # type: ignore
-    db_user.email = user_data.email # type: ignore
-    
-    await db.commit()
-    await db.refresh(db_user)
-    return db_user
+ 
+@app.get("/hello/")
+def hello(name: str = "World"):
+     name = name.strip().title()
+     return {
+         "message": f"Hello {name}"
+     }
+ 
+ 
 
+ 
 
-
-@app.delete("/users/{user_id}")
-async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(User).where(User.id == user_id))
-    user = result.scalar_one_or_none()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    await db.delete(user)
-    await db.commit()
-    return {"message": "User deleted"}
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
